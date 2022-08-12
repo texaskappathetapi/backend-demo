67d3d3f0703a02762e5b4c486ad953c7c2f4b9f7
neha <66801479+estaudere@users.noreply.github.com>
2022-08-11 22:48:55 -0500

    Condense to boilerplate

--- app/routes.py
+++ app/routes.py
@@ -11,36 +11,13 @@ def generate_short_id(num_chars):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        url = request.form['url']
-        short_id = request.form['custom_id']
-
-        if short_id and ShortURLs.query.filter_by(short_id=short_id).first() is not None:
-            flash('Please enter different custom id!')
-            return redirect(url_for('index'))
-
-        if not url:
-            flash('The URL is required!')
-            return redirect(url_for('index'))
-
-        if not short_id:
-            short_id = generate_short_id(8)
-
-        new_link = ShortURLs(
-            original_url=url, short_id=short_id, created_at=datetime.now())
-        db.session.add(new_link)
-        db.session.commit()
-        short_url = request.host_url + short_id
-
-        return render_template('index.html', short_url=short_url)
+        # implement the POST method here!
+        continue
 
     return render_template('index.html')
 
 
 @app.route('/<short_id>')
 def redirect_url(short_id):
-    link = ShortURLs.query.filter_by(short_id=short_id).first()
-    if link:
-        return redirect(link.original_url)
-    else:
-        flash('Invalid URL')
-        return redirect(url_for('index'))
+    # implement the GET method for the short_ids here!
+    pass
